635927574d816015a6c28898dfb33f86eb27d07c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-28 12:25:20 +0300

    File Handling in python

---
+++ FileHandling/files.py
@@ -0,0 +1,23 @@
+#opening and reading a file
+import os.path
+
+"""file = open("file.txt" , "r")
+for line in file:
+    print(file.readlines())
+file.close()"""
+
+#writing to a file and creating the file if it does not exist
+"""file = open("newfile.txt","w")
+file.write("Hello breshy!")
+file.close()
+print("File created successfuly..")"""
+
+#reading each line in the file
+"""file = open("newfile.txt" , "r")
+print(file.readlines())
+file.close()"""
+
+if os.path.exists("newfile.txt"):
+    os.remove("newfile.txt")
+else:
+    print("File does not exist!")
