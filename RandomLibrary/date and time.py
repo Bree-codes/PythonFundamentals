c9f9d5c68683bf1ec62f77a509c6e37baffa2c86
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-29 11:16:08 +0300

    Random, Date and Time & OS Python Libraries

---
+++ RandomLibrary/date and time.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+
+#prints current date and time
+print(datetime.datetime.now())
+
+#prints current working directory
+print(os.getcwd())
+
+#displays all files in the specified path
+print(os.listdir("/home/bree/repositories"))
