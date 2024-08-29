e68f7fa26b5ec8e248a6c1d21ab9e147be1d791c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-29 10:28:38 +0300

    Python Libraries

--- PythonLibraries/library.py
+++ PythonLibraries/library.py
@@ -8,3 +8,16 @@ print(math.pow(21,5))
 
 #factorial
 print(math.factorial(10))
+
+#sin
+print(math.sin(45))
+
+#cos
+print(math.cos(45))
+
+#tan
+print(math.tan(45))
+
+#log base 2
+print(math.log(10,2))
+print(math.log10(100))
